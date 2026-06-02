2b48d5b07e1e4e2efa4da11ccda08a2a367e1d57
zvl1380-spec <zvl1380@gmail.com>
2026-06-02 13:08:31 -0400

    Connect checkout order request endpoint

--- order-request-config.js
+++ order-request-config.js
@@ -1,5 +1,5 @@
 window.JONEZIE_ORDER_REQUEST_CONFIG = {
-  // Paste the deployed Google Apps Script Web App URL here.
-  endpoint: '',
+  // Google Apps Script Web App endpoint for automatic order request emails.
+  endpoint: 'https://script.google.com/macros/s/AKfycbwWz85m0vclkbd7w7lDN9EhbUC71X-KtwXW5Hgy3H5VLl9OOFVU3yiNGVtDhGelhw-f/exec',
   fallbackEmail: 'orders@jonezielabs.com'
 };
