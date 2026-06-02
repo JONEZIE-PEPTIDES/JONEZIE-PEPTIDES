03228aba15f97b273b0ade3cff3b11bd37b8618c
zvl1380-spec <zvl1380@gmail.com>
2026-06-02 17:27:41 -0400

    Wire lead capture endpoint

--- lead-capture-config.js
+++ lead-capture-config.js
@@ -1,4 +1,4 @@
 window.JONEZIE_LEAD_CAPTURE_CONFIG = {
-  // Paste the deployed Google Apps Script Web App URL here.
-  endpoint: ''
+  // Google Apps Script Web App endpoint for automatic lead capture notifications.
+  endpoint: 'https://script.google.com/macros/s/AKfycbwWz85m0vclkbd7w7lDN9EhbUC71X-KtwXW5Hgy3H5VLl9OOFVU3yiNGVtDhGelhw-f/exec'
 };
