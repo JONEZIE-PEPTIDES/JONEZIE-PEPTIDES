3379bd48446e17cc4772806212ffd685af285221
Jonezie Labs Publish <orders@jonezielabs.com>
2026-08-19 16:38:59 -0400

    Restore standards entry buttons and redirect

--- standards/landing-config.js
+++ standards/landing-config.js
@@ -1,4 +1,4 @@
 window.JONEZIE_RESEARCH_STANDARDS_CONFIG = {
   // Set false to hide the Explore Jonezie Labs and Enter Jonezie Labs buttons.
-  showMainSiteEntrances: false
+  showMainSiteEntrances: true
 };
