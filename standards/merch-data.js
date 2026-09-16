3ee6673e8947d7550d6da0d8fb1ae957229b6d2c
zvl1380-spec <zvl1380@gmail.com>
2026-09-16 02:05:54 -0400

    Turn off Standards redirect

--- standards/merch-data.js
+++ standards/merch-data.js
@@ -1,10 +1,10 @@
 window.JONEZIE_STANDARDS_MERCH = {
   redirect: {
     // Light-switch control for /standards/: false shows this page, true redirects to target.
-    enabled: true,
+    enabled: false,
     target: '../index.html#featured',
     delayMs: 0,
-    hideMainSiteLinksWhenDisabled: true
+    hideMainSiteLinksWhenDisabled: false
   },
   cartKey: 'jonezie_standards_merch_cart',
   orderKey: 'jonezie_standards_merch_last_order',
