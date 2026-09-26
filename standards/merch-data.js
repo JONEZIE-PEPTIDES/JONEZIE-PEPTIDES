c705132af80d6f1372f422dbc27151f8afcdbb72
Zackl <Zackl@KinneGroup.com>
2026-09-26 12:13:07 -0400

    Turn off Standards redirect

--- standards/merch-data.js
+++ standards/merch-data.js
@@ -1,7 +1,7 @@
 window.JONEZIE_STANDARDS_MERCH = {
   redirect: {
     // Light-switch control for /standards/: false shows this page, true redirects to target.
-    enabled: true,
+    enabled: false,
     target: '../index.html#featured',
     delayMs: 0,
     hideMainSiteLinksWhenDisabled: true
